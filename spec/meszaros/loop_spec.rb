c0e853fc647b1baf1ea5acb904d2f3332185174c
Bala Paranj <bparanj@gmail.com>
2012-05-17 09:40:20 -0700

    Made the specs more readable

--- spec/meszaros/loop_spec.rb
+++ spec/meszaros/loop_spec.rb
@@ -4,33 +4,33 @@ require 'meszaros/loop'
 module Meszaros
   describe Loop do
     it "should allow data driven spec : 0" do
-      x = []
+      result = []
       Loop.data_driven_spec([]) do |element|
-        x << element
+        result << element
       end
 
-      x.should == []
+      result.should be_empty
     end
     
     it "should allow data driven spec : 1" do
-      x = []
+      result = []
       Loop.data_driven_spec([4]) do |element|
-        x << element
+        result << element
       end
 
-      x.should == [4]
+      result.should == [4]
     end
     
     it "should allow data driven spec : n" do
-      x = []
+      result = []
       Loop.data_driven_spec([1,2,3,4]) do |element|
-        x << element
+        result << element
       end
 
-      x.should == [1,2,3,4]
+      result.should == [1,2,3,4]
     end
 
-    it "should raise exception when nil is passed as the parameter to data driven spec" do
+    it "should raise eresultception when nil is passed as the parameter to data driven spec" do
       expect do
         Loop.data_driven_spec(nil) do |element|
           true.should be_true
@@ -39,27 +39,27 @@ module Meszaros
 
     end
 
-    it "should allow execution of a chunk of code for 0 number of times" do
-      x = 0
+    it "should allow eresultecution of a chunk of code for 0 number of times" do
+      result = 0
       
       Loop.repeat(0) do
-        x += 1        
+        result += 1        
       end
       
-      x.should == 0
+      result.should == 0
     end
     
-    it "should allow execution of a chunk of code for 1 number of times" do
-      x = 0
+    it "should allow eresultecution of a chunk of code for 1 number of times" do
+      result = 0
       
       Loop.repeat(1) do
-        x += 1        
+        result += 1        
       end
       
-      x.should == 1
+      result.should == 1
     end
     
-    it "should raise exception when nil is passed for the parameter to repeat" do      
+    it "should raise eresultception when nil is passed for the parameter to repeat" do      
       expect do
         Loop.repeat(nil) do
           true.should be_true      
@@ -68,7 +68,7 @@ module Meszaros
       
     end
 
-    it "should raise exception when string is passed for the parameter to repeat" do      
+    it "should raise eresultception when string is passed for the parameter to repeat" do      
       expect do
         Loop.repeat("dumb") do
           true.should be_true        
@@ -76,7 +76,7 @@ module Meszaros
       end.to raise_error
     end
 
-    it "should raise exception when float is passed for the parameter to repeat" do      
+    it "should raise eresultception when float is passed for the parameter to repeat" do      
       expect do
         Loop.repeat(2.2) do
           true.should be_true        
@@ -84,14 +84,14 @@ module Meszaros
       end.to raise_error
     end
     
-    it "should allow execution of a chunk of code for n number of times" do
-      x = 0
+    it "should allow eresultecution of a chunk of code for n number of times" do
+      result = 0
       
       Loop.repeat(3) do
-        x += 1        
+        result += 1        
       end
       
-      x.should == 3
+      result.should == 3
     end
   end  
 end
